cc3b5da22ea45913c2693329c44731c9812c4615
gisuk2 <gisuk0222@gmail.com>
2026-01-12 09:13:06 +0900

    [Bronze III] Title: 전자레인지, Time: 36 ms, Memory: 32412 KB, Score: 30 point -BaekjoonHub

---
+++ 백준/Bronze/10162. 전자레인지/전자레인지.py
@@ -0,0 +1,21 @@
+import sys
+
+input = sys.stdin.readline
+
+T = int(input().strip())
+
+
+if T%10 !=0:
+    print(-1)
+
+else:
+    cnt = 0
+    li = []
+    cnt += T//300
+    li.append(cnt)
+    cnt += (T%300)//60
+    li.append(cnt)
+    cnt = 0
+    cnt += ((T%300)%60)//10
+    li.append(cnt)
+    print(*li)
