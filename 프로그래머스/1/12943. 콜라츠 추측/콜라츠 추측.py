6ab09286890cd89590922fe2f1725b241b1bf6c3
gisuk2 <gisuk0222@gmail.com>
2026-03-17 10:30:00 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.13 ms, Memory: 9.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.py
@@ -0,0 +1,16 @@
+def solution(num):
+    count = 0
+    while(True):
+        if num == 1: 
+            return count
+            break
+        if num%2==0: 
+            num = num/2
+            count+=1
+        else:
+            num = num*3 +1
+            count+=1
+        if count==500:
+            return -1
+            break
+    
